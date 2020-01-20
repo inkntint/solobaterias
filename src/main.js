ca1bb6ebdbb0eba309986115042eb8285c25a0d6
Ahsath <ahsath@gmail.com>
2020-01-20 11:18:28 -0500

    Updated

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-export default function(Vue, { router, head, isClient }) {}
+import { Carousel } from "buefy";
+import "./assets/sass/main.scss";
+
+export default function(Vue, { router, head, isClient }) {
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css"
+  });
+  Vue.use(Carousel);
+}
