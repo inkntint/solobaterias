8ba7284afdaa441fd4579496b92f1d8efec3edcf
Ahsath <ahsath@gmail.com>
2020-01-20 11:15:52 -0500

    Added purgecss

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,4 +1,24 @@
+const isProd = process.env.NODE_ENV === "production";
+const Purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["src/**/*.vue", "src/main.js"],
+  whitelistPatternsChildren: [
+    /html/,
+    /body/,
+    /hero/,
+    /icon/,
+    /slide/,
+    /carousel/
+  ]
+});
+
 module.exports = {
   siteName: "Gridsome",
-  plugins: []
+  plugins: [],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: isProd ? [Purgecss] : []
+      }
+    }
+  }
 };
