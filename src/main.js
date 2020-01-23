d51edfbb483c6e8a4aadd1e66d912f3f19af2f34
Ahsath <ahsath@gmail.com>
2020-01-23 18:48:27 -0500

    Added AppSection as global component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "circular-std";
 import "./assets/sass/main.scss";
 import "./assets/css/styles.css";
 import AppShell from "./layouts/AppShell";
+import AppSection from "./components/AppSection";
 
 export default function(Vue, { router, head, isClient }) {
   head.link.push({
@@ -11,6 +12,7 @@ export default function(Vue, { router, head, isClient }) {
       "https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css"
   });
   Vue.component("AppShell", AppShell);
+  Vue.component("AppSection", AppSection);
   Vue.use(Carousel);
   Vue.use(Icon);
   Vue.use(Autocomplete);
